10f552a9126f184b42c1f01669a224e9ade38a4d
Marviel Billante <marvielb@gmail.com>
2024-01-15 21:43:02 +0800

    Main Layout UI

--- frontend/src/components/Layout/MainLayout.tsx
+++ frontend/src/components/Layout/MainLayout.tsx
@@ -1,3 +1,23 @@
+import CarIcon from "./icons/CarIcon";
+import HistoryIcon from "./icons/HistoryIcon";
+import SearchIcon from "./icons/SearchIcon";
+
 export const MainLayout = () => {
-  return <span>This is the main layout!</span>;
+  return (
+    <div className="btm-nav btm-nav-lg">
+      <button>
+        <CarIcon />
+        <span className="btm-nav-label">Bookings</span>
+      </button>
+      <button className="active">
+        <SearchIcon />
+        <span className="btm-nav-label">Search</span>
+      </button>
+
+      <button>
+        <HistoryIcon />
+        <span className="btm-nav-label">History</span>
+      </button>
+    </div>
+  );
 };
