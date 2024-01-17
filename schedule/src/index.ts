a1e41322ef7ae592f4b3e70bbaed9ec15cdebd50
Marviel Billante <marvielb@gmail.com>
2024-01-17 20:06:46 +0800

    Initial commit for schedule service

---
+++ schedule/src/index.ts
@@ -0,0 +1,11 @@
+import cors from "@elysiajs/cors";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .use(cors())
+  .get("/health_check", () => "")
+  .listen(3002);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+);
