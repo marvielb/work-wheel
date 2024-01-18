15d879027ddc32c7982745da2b273a9e072a0bfa
Marviel Billante <marvielb@gmail.com>
2024-01-18 21:13:54 +0800

    Initial Time Departures Endpoint

--- catalog/src/index.ts
+++ catalog/src/index.ts
@@ -1,9 +1,12 @@
 import cors from '@elysiajs/cors';
 import { Elysia } from 'elysia';
+import db from '../db';
 
 const app = new Elysia()
   .use(cors())
   .get('/health_check', () => '')
+  .decorate('db', db)
+  .get('/time-departures', ({ db }) => db.selectFrom('time_departures').selectAll().execute())
   .listen(3001);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
