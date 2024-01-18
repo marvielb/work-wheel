603358b1c8483d9bbe29a89842537028fc6f63e9
Marviel Billante <marvielb@gmail.com>
2024-01-18 20:55:45 +0800

    Added seeding script

---
+++ catalog/scripts/seed-database.ts
@@ -0,0 +1,30 @@
+import db from '../db';
+
+function generateTimeIntervals(): string[] {
+  const intervals: string[] = [];
+  let currentTime = new Date();
+  currentTime.setHours(8, 0, 0, 0); // Set initial time to 8:00 AM
+
+  const endTime = new Date();
+  endTime.setHours(17, 30, 0, 0); // Set end time to 5:00 PM
+
+  while (currentTime <= endTime) {
+    const formattedTime = currentTime.toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+    intervals.push(formattedTime);
+
+    // Increment time by 30 minutes
+    currentTime.setMinutes(currentTime.getMinutes() + 30);
+  }
+
+  return intervals;
+}
+
+const timeIntervals = generateTimeIntervals();
+
+await db
+  .insertInto('time_departures')
+  .values(timeIntervals.map((v) => ({ time: v })))
+  .execute();
