7555718f7bad13be37fe4c76840efbfbb5f105a2
Marviel Billante <marvielb@gmail.com>
2024-01-23 07:57:10 +0800

    Schedule service project configuration

--- schedule/src/index.ts
+++ schedule/src/index.ts
@@ -1,11 +1,9 @@
-import cors from "@elysiajs/cors";
-import { Elysia } from "elysia";
+import cors from '@elysiajs/cors';
+import { Elysia } from 'elysia';
 
 const app = new Elysia()
   .use(cors())
-  .get("/health_check", () => "")
+  .get('/health_check', () => '')
   .listen(3002);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
--- schedule/src/lib/api.ts
+++ schedule/src/lib/api.ts
@@ -1,5 +1,5 @@
-import createClient from "openapi-fetch";
-import { paths as catalogPaths } from "./api/catalog";
+import createClient from 'openapi-fetch';
+import { paths as catalogPaths } from './api/catalog';
 
 export const catalogClient = createClient<catalogPaths>({
   baseUrl: Bun.env.CATALOG_SERVICE_OPEN_API_JSON_URL,
